4d64fcdebcae8bbe163412956843d14888ca96e5
Noa Barak <banoa@post.bgu.ac.il>
2022-12-13 23:09:32 +0200

    naara hasmalit

--- src/main/java/bguspl/set/ex/Dealer.java
+++ src/main/java/bguspl/set/ex/Dealer.java
@@ -36,7 +36,7 @@ public class Dealer implements Runnable {
     /**
      * The time when the dealer needs to reshuffle the deck due to turn timeout.
      */
-    private long reshuffleTime = Long.MAX_VALUE;
+    private long reshuffleTime = 0;
 
     private Queue<Player> playersQueue;
 
@@ -69,12 +69,14 @@ public class Dealer implements Runnable {
      * The inner loop of the dealer thread that runs as long as the countdown did not time out.
      */
     private void timerLoop() {
+        reshuffleTime = System.currentTimeMillis() + 60000;
         while (!terminate && System.currentTimeMillis() < reshuffleTime) {
             sleepUntilWokenOrTimeout();
             updateTimerDisplay(false);
             removeCardsFromTable();
             placeCardsOnTable();
         }
+
     }
 
     /**
@@ -151,7 +153,9 @@ public class Dealer implements Runnable {
      * Reset and/or update the countdown and the countdown display.
      */
     private void updateTimerDisplay(boolean reset) {
-        // TODO implement
+        long currentTime = System.currentTimeMillis();
+            env.ui.setCountdown(reshuffleTime-currentTime+1000,false);
+
     }
 
     /**
